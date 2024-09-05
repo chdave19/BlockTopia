5efc96ded22f172b951108e054ab8fd4244a95c9
Chigozie David <chigoziedavid@Chigozies-MacBook-Air.local>
2024-09-05 16:09:21 +0100

    update max 5.5

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import styled from 'styled-components';
 import './App.css';
 import Game from './js/Game';
+import { useEffect, useState } from 'react';
 
+const OpenScreen = styled.div`
+ height: 100vh;
+ width: 100vw;
+ background-color: #000;
+ display: ${props=>props.$fullscreen?'none':'grid'};
+ place-content: center;
+ position: fixed;
+ inset: 0;
+ z-index: 9999;
+ button{
+  padding: 1rem;
+ }
+`
 function App() {
+  const [openFullScreen, setScreen] = useState(false);
+  function fullScreen(){
+    const elem = document.documentElement;
+    if(elem){
+      elem.requestFullscreen();
+      setScreen(true);
+    }
+  }
+  useEffect(()=>{
+    
+  }, [])
   return (
-   <Game/>
+   <>
+   <OpenScreen $fullscreen={openFullScreen}>
+    <button onClick={()=>fullScreen()}>Play</button>
+   </OpenScreen>
+   {
+    openFullScreen && <Game/>
+   }
+   </>
   );
 }
 
